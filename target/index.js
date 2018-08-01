3e054c5c1ac5615b79c4b49030012e1d277f3a82
Jose Mesa <jose.mesa.solorzano@gmail.com>
2018-08-01 14:12:11 +0200

    ss

--- target/index.js
+++ target/index.js
@@ -8,6 +8,10 @@ const http_1 = require("http");
 const IO = require("socket.io");
 const controller_1 = require("./sessions/controller");
 const controller_2 = require("./turns/controller");
+const routing_controllers_2 = require("routing-controllers");
+const entity_1 = require("./sessions/entity");
+const entity_2 = require("./turns/entity");
+const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
 const app = new Koa();
 const server = new http_1.Server(app.callback());
 exports.io = IO(server);
@@ -20,7 +24,62 @@ routing_controllers_1.useKoaServer(app, {
     ]
 });
 exports.io.on('connect', (socket) => {
-    console.log('connection made , socket id --->', socket.id);
+    socket.on('NEW_DB', async (sessionId, participantId, sample) => {
+        const session = await entity_1.Session.findOne(sessionId);
+        if (!session)
+            throw new routing_controllers_2.NotFoundError('Session not found');
+        if (session.status !== 'started')
+            throw new routing_controllers_2.ForbiddenError("the sessison hasn't started yet");
+        const participant = await entity_1.Participant.findOne(participantId);
+        if (!participant)
+            throw new routing_controllers_2.NotFoundError('You are not part of this session');
+        participant.avgDecibels = average(sample);
+        await participant.save();
+        const [{ "max": maxAvg }] = await entity_1.Participant.query(`select MAX(avg_decibels) from participants where session_id=${sessionId}`);
+        const [speaker] = await entity_1.Participant.query(`select * from participants where avg_decibels=${maxAvg} and session_id=${sessionId}`);
+        if (participant.avgDecibels > -20 && speaker.id === participantId && participant.participantStatus === 'inactive') {
+            const turn = await entity_2.default.create();
+            turn.session = session;
+            turn.participant = participant;
+            const startTime = new Date().toISOString();
+            turn.startTime = startTime;
+            const newTurn = await turn.save();
+            participant.lastTurnId = newTurn.id;
+            participant.participantStatus = 'active';
+            const updatedParticipant = await participant.save();
+            const [payload] = await entity_1.Participant.query(`select * from participants where id=${updatedParticipant.id}`);
+            exports.io.emit('UPDATE_PARTICIPANT', payload);
+            return payload;
+        }
+        if (participant.avgDecibels < -20 && participant.participantStatus === 'active' || participant.avgDecibels > 20 && speaker.id !== participantId && participant.participantStatus === 'active') {
+            console.log('working');
+            const turn = await entity_2.default.findOne(participant.lastTurnId);
+            if (!turn)
+                throw new routing_controllers_2.BadRequestError('turn entity not found');
+            const endTime = new Date().toISOString();
+            turn.endTime = endTime;
+            await turn.save();
+            const timeSpoken = Math.round((new Date(turn.endTime).getTime() - new Date(turn.startTime).getTime()) / 1000);
+            participant.timeSpeakingSeconds = participant.timeSpeakingSeconds + timeSpoken;
+            if (participant.timeSpeakingSeconds > session.timePerPiece && participant.timeSpeakingSeconds <= 5 * session.timePerPiece) {
+                participant.numberOfPieces = 5 - Math.trunc(participant.timeSpeakingSeconds / session.timePerPiece);
+            }
+            else if (participant.timeSpeakingSeconds > 5 * session.timePerPiece) {
+                participant.numberOfPieces = 0;
+            }
+            participant.participantStatus = 'inactive';
+            const updatedParticipant = await participant.save();
+            const [payload] = await entity_1.Participant.query(`select * from participants where id=${updatedParticipant.id}`);
+            exports.io.emit('UPDATE_PARTICIPANT', payload);
+            const [{ 'sum': sumpayload }] = await entity_1.Participant.query(`SELECT SUM(number_of_pieces) FROM participants where session_id=${session.id}`);
+            session.piecesToComplete = sumpayload;
+            await session.save();
+            const [updatedSession] = await entity_1.Session.query(`select * from sessions where id=${session.id}`);
+            exports.io.emit('UPDATE_SESSION', updatedSession);
+            return payload;
+        }
+        return speaker;
+    });
 });
 db_1.default()
     .then(_ => {
