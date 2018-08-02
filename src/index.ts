d95499b32b6cc4aa53f789ac01077d266d730f2b
Jose Mesa <jose.mesa.solorzano@gmail.com>
2018-08-02 12:46:15 +0200

    fixind async

--- src/index.ts
+++ src/index.ts
@@ -29,100 +29,105 @@ io.on('connect', (socket) => {
   
 
     socket.on('NEW_DB',  async (payload) => {
-      const {sessionId , participantId , buffer} = payload
-      const session = await Session.findOne(sessionId)
-      if(!session) throw new NotFoundError('Session not found')
-      if(session.status !== 'started') throw new ForbiddenError("the sessison hasn't started yet")
-  
-      const participant = await Participant.findOne(participantId)
-      if(!participant) throw new NotFoundError('You are not part of this session')
-  
-      participant.avgDecibels = average(buffer)
-      await participant.save()
-  
-      const [{"max": maxAvg}] = await Participant.query(`select MAX(avg_decibels) from participants where session_id=${sessionId}`)
-      const [speaker] = await Participant.query(`select * from participants where avg_decibels=${maxAvg} and session_id=${sessionId}`)
-  
-      if(participant.avgDecibels > -25 && speaker.id === participantId && participant.participantStatus === 'inactive') {
-          
-          const turn =  await Turn.create()
-          
-  
-          turn.session = session
-          turn.participant = participant
-  
-          
-          const startTime = new Date().toISOString()
-          turn.startTime = startTime
-          
-          
-          const newTurn = await turn.save()
-  
-          participant.lastTurnId = newTurn.id
-          participant.participantStatus = 'active'
-  
-          const updatedParticipant = await participant.save()
-  
-          // const [payload] = await Participant.query(`select * from participants where id=${updatedParticipant.id}`)
-  
-          // io.emit('UPDATE_PARTICIPANT', payload)
-  
-          // return payload
-  
-  
-      }
-  
-      if(participant.avgDecibels < -25 && participant.participantStatus === 'active' || participant.avgDecibels > 20 && speaker.id !== participantId && participant.participantStatus === 'active') {
-          console.log('working')
-          const turn = await Turn.findOne(participant.lastTurnId)
-          if(!turn) throw new BadRequestError('turn entity not found')
-  
-          const endTime = new Date().toISOString()
-          turn.endTime = endTime
-          await turn.save()
-  
-          const timeSpoken =  Math.round((new Date(turn.endTime).getTime() - new Date(turn.startTime).getTime())/1000)
-  
-          participant.timeSpeakingSeconds = participant.timeSpeakingSeconds + timeSpoken
-          
-          if(participant.timeSpeakingSeconds > session.timePerPiece && participant.timeSpeakingSeconds <= 5*session.timePerPiece){
-              participant.numberOfPieces = 5 - Math.trunc(participant.timeSpeakingSeconds/session.timePerPiece)
-          }else if(participant.timeSpeakingSeconds > 5*session.timePerPiece) {
-              participant.numberOfPieces = 0
-  
-          }
-  
-          participant.participantStatus = 'inactive'
-          const updatedParticipant = await participant.save()
-  
-          const [payload] = await Participant.query(`select * from participants where id=${updatedParticipant.id}`)
-  
-          io.emit('UPDATE_PARTICIPANT', payload)
-  
-          io.emit('action', {
-            type: 'UPDATE_PARTICIPANT',
-            payload: payload
-          })
-  
-  
-          const [{'sum': sumpayload}] = await Participant.query(`SELECT SUM(number_of_pieces) FROM participants where session_id=${session.id}`)
-  
-          session.piecesToComplete = sumpayload
-          await session.save()
-  
-          const [updatedSession] =await Session.query(`select * from sessions where id=${session.id}`)
-  
-          io.emit( 'UPDATE_SESSION', updatedSession )
+      try {
+        const {sessionId , participantId , buffer} = payload
+        const session = await Session.findOne(sessionId)
+        if(!session) throw new NotFoundError('Session not found')
+        if(session.status !== 'started') throw new ForbiddenError("the sessison hasn't started yet")
+    
+        const participant = await Participant.findOne(participantId)
+        if(!participant) throw new NotFoundError('You are not part of this session')
+    
+        participant.avgDecibels = average(buffer)
+        await participant.save()
+    
+        const [{"max": maxAvg}] = await Participant.query(`select MAX(avg_decibels) from participants where session_id=${sessionId}`)
+        const [speaker] = await Participant.query(`select * from participants where avg_decibels=${maxAvg} and session_id=${sessionId}`)
+    
+        if(participant.avgDecibels > -25 && speaker.id === participantId && participant.participantStatus === 'inactive') {
+            
+            const turn =  await Turn.create()
+            
+    
+            turn.session = session
+            turn.participant = participant
+    
+            
+            const startTime = new Date().toISOString()
+            turn.startTime = startTime
+            
+            
+            const newTurn = await turn.save()
+    
+            participant.lastTurnId = newTurn.id
+            participant.participantStatus = 'active'
+    
+            const updatedParticipant = await participant.save()
+    
+            // const [payload] = await Participant.query(`select * from participants where id=${updatedParticipant.id}`)
+    
+            // io.emit('UPDATE_PARTICIPANT', payload)
+    
+            // return payload
+    
+    
+        }
+    
+        if(participant.avgDecibels < -25 && participant.participantStatus === 'active' || participant.avgDecibels > 20 && speaker.id !== participantId && participant.participantStatus === 'active') {
+            console.log('working')
+            const turn = await Turn.findOne(participant.lastTurnId)
+            if(!turn) throw new BadRequestError('turn entity not found')
+    
+            const endTime = new Date().toISOString()
+            turn.endTime = endTime
+            await turn.save()
+    
+            const timeSpoken =  Math.round((new Date(turn.endTime).getTime() - new Date(turn.startTime).getTime())/1000)
+    
+            participant.timeSpeakingSeconds = participant.timeSpeakingSeconds + timeSpoken
+            
+            if(participant.timeSpeakingSeconds > session.timePerPiece && participant.timeSpeakingSeconds <= 5*session.timePerPiece){
+                participant.numberOfPieces = 5 - Math.trunc(participant.timeSpeakingSeconds/session.timePerPiece)
+            }else if(participant.timeSpeakingSeconds > 5*session.timePerPiece) {
+                participant.numberOfPieces = 0
+    
+            }
+    
+            participant.participantStatus = 'inactive'
+            const updatedParticipant = await participant.save()
+    
+            const [payload] = await Participant.query(`select * from participants where id=${updatedParticipant.id}`)
+    
+            io.emit('UPDATE_PARTICIPANT', payload)
+    
+            io.emit('action', {
+              type: 'UPDATE_PARTICIPANT',
+              payload: payload
+            })
+    
+    
+            const [{'sum': sumpayload}] = await Participant.query(`SELECT SUM(number_of_pieces) FROM participants where session_id=${session.id}`)
+    
+            session.piecesToComplete = sumpayload
+            await session.save()
+    
+            const [updatedSession] =await Session.query(`select * from sessions where id=${session.id}`)
+    
+            io.emit( 'UPDATE_SESSION', updatedSession )
 
-          io.emit('action', {
-            type: 'UPDATE_SESSION',
-            payload: updatedSession
-          })
-  
-          // return payload
+            io.emit('action', {
+              type: 'UPDATE_SESSION',
+              payload: updatedSession
+            })
+    
+            // return payload
+        }
+    
+        // return speaker
+      } catch (error) {
+        console.error(error);
       }
-  
-      // return speaker
+      
       
     });
 
--- target/index.js
+++ target/index.js
@@ -25,63 +25,68 @@ routing_controllers_1.useKoaServer(app, {
 });
 exports.io.on('connect', (socket) => {
     socket.on('NEW_DB', async (payload) => {
-        const { sessionId, participantId, buffer } = payload;
-        const session = await entity_1.Session.findOne(sessionId);
-        if (!session)
-            throw new routing_controllers_2.NotFoundError('Session not found');
-        if (session.status !== 'started')
-            throw new routing_controllers_2.ForbiddenError("the sessison hasn't started yet");
-        const participant = await entity_1.Participant.findOne(participantId);
-        if (!participant)
-            throw new routing_controllers_2.NotFoundError('You are not part of this session');
-        participant.avgDecibels = average(buffer);
-        await participant.save();
-        const [{ "max": maxAvg }] = await entity_1.Participant.query(`select MAX(avg_decibels) from participants where session_id=${sessionId}`);
-        const [speaker] = await entity_1.Participant.query(`select * from participants where avg_decibels=${maxAvg} and session_id=${sessionId}`);
-        if (participant.avgDecibels > -25 && speaker.id === participantId && participant.participantStatus === 'inactive') {
-            const turn = await entity_2.default.create();
-            turn.session = session;
-            turn.participant = participant;
-            const startTime = new Date().toISOString();
-            turn.startTime = startTime;
-            const newTurn = await turn.save();
-            participant.lastTurnId = newTurn.id;
-            participant.participantStatus = 'active';
-            const updatedParticipant = await participant.save();
+        try {
+            const { sessionId, participantId, buffer } = payload;
+            const session = await entity_1.Session.findOne(sessionId);
+            if (!session)
+                throw new routing_controllers_2.NotFoundError('Session not found');
+            if (session.status !== 'started')
+                throw new routing_controllers_2.ForbiddenError("the sessison hasn't started yet");
+            const participant = await entity_1.Participant.findOne(participantId);
+            if (!participant)
+                throw new routing_controllers_2.NotFoundError('You are not part of this session');
+            participant.avgDecibels = average(buffer);
+            await participant.save();
+            const [{ "max": maxAvg }] = await entity_1.Participant.query(`select MAX(avg_decibels) from participants where session_id=${sessionId}`);
+            const [speaker] = await entity_1.Participant.query(`select * from participants where avg_decibels=${maxAvg} and session_id=${sessionId}`);
+            if (participant.avgDecibels > -25 && speaker.id === participantId && participant.participantStatus === 'inactive') {
+                const turn = await entity_2.default.create();
+                turn.session = session;
+                turn.participant = participant;
+                const startTime = new Date().toISOString();
+                turn.startTime = startTime;
+                const newTurn = await turn.save();
+                participant.lastTurnId = newTurn.id;
+                participant.participantStatus = 'active';
+                const updatedParticipant = await participant.save();
+            }
+            if (participant.avgDecibels < -25 && participant.participantStatus === 'active' || participant.avgDecibels > 20 && speaker.id !== participantId && participant.participantStatus === 'active') {
+                console.log('working');
+                const turn = await entity_2.default.findOne(participant.lastTurnId);
+                if (!turn)
+                    throw new routing_controllers_2.BadRequestError('turn entity not found');
+                const endTime = new Date().toISOString();
+                turn.endTime = endTime;
+                await turn.save();
+                const timeSpoken = Math.round((new Date(turn.endTime).getTime() - new Date(turn.startTime).getTime()) / 1000);
+                participant.timeSpeakingSeconds = participant.timeSpeakingSeconds + timeSpoken;
+                if (participant.timeSpeakingSeconds > session.timePerPiece && participant.timeSpeakingSeconds <= 5 * session.timePerPiece) {
+                    participant.numberOfPieces = 5 - Math.trunc(participant.timeSpeakingSeconds / session.timePerPiece);
+                }
+                else if (participant.timeSpeakingSeconds > 5 * session.timePerPiece) {
+                    participant.numberOfPieces = 0;
+                }
+                participant.participantStatus = 'inactive';
+                const updatedParticipant = await participant.save();
+                const [payload] = await entity_1.Participant.query(`select * from participants where id=${updatedParticipant.id}`);
+                exports.io.emit('UPDATE_PARTICIPANT', payload);
+                exports.io.emit('action', {
+                    type: 'UPDATE_PARTICIPANT',
+                    payload: payload
+                });
+                const [{ 'sum': sumpayload }] = await entity_1.Participant.query(`SELECT SUM(number_of_pieces) FROM participants where session_id=${session.id}`);
+                session.piecesToComplete = sumpayload;
+                await session.save();
+                const [updatedSession] = await entity_1.Session.query(`select * from sessions where id=${session.id}`);
+                exports.io.emit('UPDATE_SESSION', updatedSession);
+                exports.io.emit('action', {
+                    type: 'UPDATE_SESSION',
+                    payload: updatedSession
+                });
+            }
         }
-        if (participant.avgDecibels < -25 && participant.participantStatus === 'active' || participant.avgDecibels > 20 && speaker.id !== participantId && participant.participantStatus === 'active') {
-            console.log('working');
-            const turn = await entity_2.default.findOne(participant.lastTurnId);
-            if (!turn)
-                throw new routing_controllers_2.BadRequestError('turn entity not found');
-            const endTime = new Date().toISOString();
-            turn.endTime = endTime;
-            await turn.save();
-            const timeSpoken = Math.round((new Date(turn.endTime).getTime() - new Date(turn.startTime).getTime()) / 1000);
-            participant.timeSpeakingSeconds = participant.timeSpeakingSeconds + timeSpoken;
-            if (participant.timeSpeakingSeconds > session.timePerPiece && participant.timeSpeakingSeconds <= 5 * session.timePerPiece) {
-                participant.numberOfPieces = 5 - Math.trunc(participant.timeSpeakingSeconds / session.timePerPiece);
-            }
-            else if (participant.timeSpeakingSeconds > 5 * session.timePerPiece) {
-                participant.numberOfPieces = 0;
-            }
-            participant.participantStatus = 'inactive';
-            const updatedParticipant = await participant.save();
-            const [payload] = await entity_1.Participant.query(`select * from participants where id=${updatedParticipant.id}`);
-            exports.io.emit('UPDATE_PARTICIPANT', payload);
-            exports.io.emit('action', {
-                type: 'UPDATE_PARTICIPANT',
-                payload: payload
-            });
-            const [{ 'sum': sumpayload }] = await entity_1.Participant.query(`SELECT SUM(number_of_pieces) FROM participants where session_id=${session.id}`);
-            session.piecesToComplete = sumpayload;
-            await session.save();
-            const [updatedSession] = await entity_1.Session.query(`select * from sessions where id=${session.id}`);
-            exports.io.emit('UPDATE_SESSION', updatedSession);
-            exports.io.emit('action', {
-                type: 'UPDATE_SESSION',
-                payload: updatedSession
-            });
+        catch (error) {
+            console.error(error);
         }
     });
 });
